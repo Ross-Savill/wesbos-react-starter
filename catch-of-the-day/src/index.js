5ee0a36e1c882b7984d7fa8dc8fcc1045312fcaa
Ross Savill <rosssavill252@hotmail.com>
2019-02-03 15:29:07 +1100

    up to 6

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,7 @@
-import React from 'react';
+import React from 'react'
+import { Component } from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker'
+import "./css/style.css"
+
+render(<StorePicker />, document.querySelector('#main'))
