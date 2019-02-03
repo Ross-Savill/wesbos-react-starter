1539bb6b2db3696eff0bf399da6816f5ed185276
Ross Savill <rosssavill252@hotmail.com>
2019-02-03 21:10:17 +1100

    state lesson done

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,24 @@ import Inventory from './Inventory'
 import Order from './Order'
 
 class App extends React.Component {
+    state = {
+        fishes: {},
+        order: {}
+    }
+    addFish = (fish) => {
+        const fishes = { ...this.state.fishes }
+        fishes[`fish${Date.now()}`] = fish
+        this.setState({ fishes })
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                 <Header tagline="Fresh Food Market"/>
                 </div>
-                <Inventory />
                 <Order />
+                <Inventory addFish = {this.addFish}/>
             </div>
         )
     }
