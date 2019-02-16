f52545eae2f62d996fce345a29e243b7a2fe8f96
Ross Savill <rosssavill252@hotmail.com>
2019-02-16 20:13:40 +1100

    up to 20

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,15 +4,42 @@ import Inventory from './Inventory'
 import Order from './Order'
 import sampleFishes from '../sample-fishes'
 import Fish from "./Fish"
+import base from "../base"
 
 class App extends React.Component {
     state = {
         fishes: {},
         order: {}
     }
+
+    componentDidMount() {
+        // first reinstate local storage
+        const { params } = this.props.match
+        const localStorageRef = localStorage.getItem(params.storeId)
+        
+        if (localStorageRef) {
+            this.setState ({ order: JSON.parse(localStorageRef)})
+        }
+        this.ref = base.syncState(`${params.storeId}/fishes`, {
+            context: this,
+            state: 'fishes'
+        })
+    }
+
+    componentDidUpdate() {
+        localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order))
+    }
+
+    componentWillUnmount() {
+        base.removeBinding(this.ref)
+    }
+
     addFish = (fish) => {
+        // copy state
         const fishes = { ...this.state.fishes }
+        // add fish to the existing state
         fishes[`fish${Date.now()}`] = fish
+        // set new fishes object to state 
         this.setState({ fishes })
     }
 
