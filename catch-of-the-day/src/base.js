4304994cb8f7e77e04cc67a55b9a812a8f709a0b
Ross Savill <rosssavill252@hotmail.com>
2019-03-18 01:59:42 +1100

    fixed dotenv

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,13 +1,12 @@
 import Rebase from 're-base'
 import firebase from "firebase"
-require('dotenv').config({ debug: process.env.DEBUG })
+require('dotenv').config()
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DATABASEURL
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL
 })
-console.log(firebaseApp.AUTHDOMAIN)
 
 const base = Rebase.createClass(firebaseApp.database())
 
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import { render } from 'react-dom';
 import "./css/style.css"
 import Router from './components/Router'
+require('dotenv').config()
 
 render(<Router />, document.querySelector("#main"))
