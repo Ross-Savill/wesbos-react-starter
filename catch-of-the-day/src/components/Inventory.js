0e63aab0ea63f524f77ecc9aac2dd9944a0fb95d
Ross Savill <rosssavill252@hotmail.com>
2019-03-21 23:03:38 +1100

    half way through auth

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import firebase from 'firebase'
 import AddFishForm from './AddFishForm'
 import EditFishForm from './EditFishForm'
-import PropTypes from 'prop-types'
+import Login from './Login'
+import { firebaseApp } from '../base';
 
 class Inventory extends React.Component {
 	static propTypes = {
@@ -9,25 +12,31 @@ class Inventory extends React.Component {
 		updateFish: PropTypes.func,
 		deleteFish: PropTypes.func,
 		loadSampleFishes: PropTypes.func
+	};
+
+	authenticate = (provider) => {
+		const authProvider = new firebase.auth()
 	}
-    render() {
-        return (
-            <div className="inventory">
-							<h2>Inventory</h2>
-							{Object.keys(this.props.fishes).map(key => (
-								<EditFishForm
-									key={key}
-									index={key}
-									fish={this.props.fishes[key]}
-									updateFish = {this.props.updateFish}
-									deleteFish={this.props.deleteFish}
-								/>
-							))}
-            <AddFishForm addFish = {this.props.addFish} />
-            <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
-            </div>
-        )
-    }
+
+  render() {
+		return <Login authenticate={this.authenticate}/>;
+    return (
+      <div className="inventory">
+			<h2>Inventory</h2>
+			{Object.keys(this.props.fishes).map(key => (
+			<EditFishForm
+				key={key}
+				index={key}
+				fish={this.props.fishes[key]}
+				updateFish = {this.props.updateFish}
+				deleteFish={this.props.deleteFish}
+			/>
+			))}
+      <AddFishForm addFish = {this.props.addFish} />
+      <button onClick={this.props.loadSampleFishes}>Load Sample Fishes</button>
+      </div>
+    )
+  }
 }
 
 export default Inventory;
