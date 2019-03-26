a25e94e2536c33f93e8c054008699ba165b451bc
Ross Savill <rosssavill252@hotmail.com>
2019-03-27 01:39:37 +1100

    react done

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom';
-import "./css/style.css"
 import Router from './components/Router'
-require('dotenv').config()
+import "./css/style.css"
 
 render(<Router />, document.querySelector("#main"))
